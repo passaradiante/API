2fc813dfe7f709ae6b89e9ddd22bba3eb0b970f2
sholiveira <sholiveira.git@gmail.com>
2019-10-10 01:43:03 -0300

    Adicionar arquivos de projeto.

---
+++ Models/UsuarioContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class UsuarioContext : DbContext
+    {
+        public UsuarioContext(DbContextOptions<UsuarioContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<UsuarioDominio> Usuarios { get; set; }
+    }
+}
